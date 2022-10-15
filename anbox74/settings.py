968fdd982b437019a4f6cf37ddbc953d106fe9b0
Хлыбов А.Г <AGKhlybov@sberbank.ru>
2022-10-16 01:22:28 +0300

    START: добавлены данные для пром

--- anbox74/settings.py
+++ anbox74/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = "/static/"
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR.joinpath('static'),]
 else:
-    STATIC_ROOT = BASE_DIR.joinpath('static')
+    STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
